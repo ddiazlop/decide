a36dc4dc81e0881c6af6afad19ee1db627bb7708
Diego <diewentdiaz@gmail.com>
2022-10-11 10:53:12 +0200

    cambios mios hola

---
+++ decide/pyEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/diego/Desktop/EGC/Practicas/decideCodeP1/decide/pyEnv/bin/python3.8
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
