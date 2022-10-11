41b4cf843eee7c7797ec8481ef93521964b7afaa
Diego <diewentdiaz@gmail.com>
2022-10-11 12:02:40 +0200

    Otra wf

---
+++ decide/local_settings.gactions.py
@@ -0,0 +1,13 @@
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'decide',
+        'USER': 'decide',
+	'PASSWORD': 'decide',
+        'HOST': '127.0.0.1',
+	'PORT': '5432'
+    }
+}
+
